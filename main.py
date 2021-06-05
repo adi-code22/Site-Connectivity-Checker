4bd1423df90a1928c5de91fdc7d864d52c3178d5
Adityakrishnan <adityakrishnanp007@gmail.com>
2021-06-05 16:36:05 +0530

    Added count, added modulus principle to round the loop

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-
 import requests, time
+import cv2
 
 print("\n---> This is a Site Connectivity Checker <---\n")
 print("Input your URL in the following format 'http://www.google.com'")
+url = 1
+diff = 1
 
-
-def comin():
+def initialize():
     url = input("Enter a URL to check: ")
     diff = input(f"Ping {url} on an interval of (seconds): ")
+    count = input(f"Ping {url} for how many times (number of times): ")
+    return url, diff, count
 
-    return url, diff
-url, diff = comin()
 
+url, diff, count = initialize()
+print("")
+flag = 1
 # response = requests.get(url)
 while True:
 
     try:
+        if flag % (int(count) + 1) == 0:
+            print(f"\n{url} was pinged for {count} times")
+            print("")
+            url, diff, count = initialize()
+            print("")
+
         response = requests.get(url)
         status_code = response.status_code
         # print("Enter 'Q' to stop")
@@ -28,14 +38,17 @@ while True:
         if status_code == 404:
             print(f"{url} is UNAVAILABLE, the site was NOT FOUND and responded with a status code {status_code}")
         if status_code == 500:
-            print(f"{url} is UNAVAILABLE, the site is having an INTERNAL SERVER ERROR and responded with a status code {status_code}")
+            print(
+                f"{url} is UNAVAILABLE, the site is having an INTERNAL SERVER ERROR and responded with a status code {status_code}")
         if status_code == 503:
-            print(f"{url} is UNAVAILABLE, the site's SERVICE is UNAVAILABLE and responded with a status code {status_code}")
+            print(
+                f"{url} is UNAVAILABLE, the site's SERVICE is UNAVAILABLE and responded with a status code {status_code}")
 
 
     except Exception as e:
         print(e)
         url = input("\nEnter another URL to check: ")
-        diff = input("Ping the following site on an interval of (seconds): ")
-
+        diff = input(f"Ping {url} on an interval of (seconds): ")
+        count = input(f"Ping {url} for how many times (number of times): ")
     time.sleep(int(diff))
+    flag = flag + 1
